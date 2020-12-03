fd0d63b285bc154077f71d74ce5d4f62f0189c02
SpaceWarf <space_warf@live.ca>
2020-12-03 00:22:10 -0500

    Add day 3 solutions

---
+++ src/day-3/code.js
@@ -0,0 +1,29 @@
+import { input } from './input';
+
+export const solution1 = (input, downModifier, rightModifier) => {
+    const lineCount = input.length;
+    const squarePerLine = input[0].length;
+    let rightPos = 0;
+    let downPos = 0;
+    let treeCounter = 0;
+
+    while (downPos < lineCount) {
+        treeCounter += input[downPos % lineCount][rightPos % squarePerLine] === "#"
+            ? 1 : 0;
+        rightPos += rightModifier;
+        downPos += downModifier;
+    }
+
+    return treeCounter; //?+
+};
+
+export const solution2 = input => {
+    return solution1(input, 1, 1)
+        * solution1(input, 1, 3)
+        * solution1(input, 1, 5)
+        * solution1(input, 1, 7)
+        * solution1(input, 2, 1) //?+
+};
+
+solution1(input, 1, 3);
+solution2(input);
---
+++ src/day-3/code.spec.js
@@ -0,0 +1,15 @@
+import { input } from './input';
+import {
+    solution1,
+    solution2
+} from './code';
+
+describe('Day 1', () => {
+    it('Puzzle 1 should return the result 151', () => {
+        expect(solution1(input, 1, 3)).toEqual(151);
+    });
+
+    it('Puzzle 2 should return the result 7540141059', () => {
+        expect(solution2(input)).toEqual(7540141059);
+    });
+});
